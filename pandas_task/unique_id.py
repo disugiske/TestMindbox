76c4989367ac7d7b90a04be669ad77538781e2cb
disugiske <disugiske@yandex.ru>
2022-11-18 20:42:01 +0300

    Remove Chunks

--- pandas_task/unique_id.py
+++ pandas_task/unique_id.py
@@ -1,25 +1,25 @@
+import time
+
 import pandas as pd
 from datetime import datetime
 
 filename = 'frame_result.csv'
-chunksize = 15000
 
 
 def format_frame():
     date_parse_func = lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S")
-    frame = pd.read_csv('test_data.csv',
+    frame = pd.read_csv('test_data_200.csv',
                         dtype={'customer_id': pd.Int32Dtype(),
                                'product_id': pd.Int32Dtype(),
                                },
-                        chunksize=chunksize,
                         parse_dates=['timestamp'],
                         date_parser=date_parse_func,
                         )
-    return frame
+    yield frame
 
 
-def main():
-    for frame in format_frame():
+def main(frames):
+    for frame in frames:
         delta = lambda t: t.diff().gt(pd.Timedelta('3min')).cumsum()
         frame['session_id'] = frame.groupby(['customer_id'], group_keys=False)['timestamp'].apply(delta).astype(
             'uint32')
@@ -29,4 +29,6 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    start = time.time()
+    main(format_frame())
+    print(time.time() - start)
